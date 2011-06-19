4c3f1f6bf098f364107ec635aec6e8dca7e4645f
James A. Rosen <james@zendesk.com>
2011-06-19 14:37:05 -0700

    Don't require tree. Closes #2.
    If tree is unavailable, prints a warning instead of trying (and failing) to
    use to get information about the asset-build.

--- lib/guard/rails-assets.rb
+++ lib/guard/rails-assets.rb
@@ -29,10 +29,7 @@ module Guard
       puts 'Compiling rails assets'
       result = system "rm -rf #{assets_dir} && bundle exec rake assets:precompile"
       if result
-        tree = `tree #{assets_dir}`
-        puts tree
-        summary = tree.split("\n").last
-        Notifier::notify summary, :title => 'Assets compiled'
+        Notifier::notify success_summary, :title => 'Assets compiled'
       else
         Notifier::notify 'see the details in the terminal', :title => "Can't compile assets", :image => :failed
       end
@@ -49,6 +46,21 @@ module Guard
       run_on = [:start, :all, :change] if not run_on or run_on.empty? 
       run_on = [run_on] unless run_on.respond_to?(:include?)
       run_on.include?(command)
+    end
+
+    def success_summary
+      if system_has_tree?
+        tree = `tree #{assets_dir}`
+        puts tree
+        tree.split("\n").last
+      else
+        '(Install tree for additional information)'
+      end
+    end
+
+    def system_has_tree?
+      return @has_tree if instance_variable_defined?(:@has_tree)
+      @has_tree = system "which tree"
     end
   end
 end
